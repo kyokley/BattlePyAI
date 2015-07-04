0ca207be3f8d1d7810ba5d8b7dcb5483d24d4be0
Kevin Yokley <kyokley2@gmail.com>
2015-07-04 08:35:56 -0500

    Adding options for board visualization

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from battlePy.series import Series
 from random_player import RandomPlayer
 
 def main():
-    series = Series(RandomPlayer(), RandomPlayer(), 1000)
+    series = Series(RandomPlayer(),
+                    RandomPlayer(),
+                    numberOfGames=1000,
+                    showVisualization=False,
+                    visualizationInterval=.01)
     series.start()
-    series.printStats()
 
 if __name__ == '__main__':
     main()
